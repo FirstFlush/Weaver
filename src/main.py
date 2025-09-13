d3bdac13cf1702273e448fc14026df699f2cad15
FirstFlush <firstflush@protonmail.com>
2025-09-12 20:20:53 -0700

    feat: added logging

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+import logging.config
+from .config.logging import LOGGING
+
+logging.config.dictConfig(LOGGING)
 
 app = FastAPI()
+logger = logging.getLogger(__name__)
 
 @app.get("/ping")
 def ping():
     return {"ping": "PONG"}
-
-
 
 @app.post("/scrape")
 def scrape():
