0ad3207ad2f71608a242b9a68a29c65d3f35977a
FirstFlush <firstflush@protonmail.com>
2025-09-17 21:57:56 -0700

    refactor: update default headers to mimic modern browser patterns

--- weaver/common/utils.py
+++ weaver/common/utils.py
@@ -7,9 +7,17 @@ def create_ua() -> str:
 def generate_default_headers() -> dict[str, str]:
     return {
         'User-Agent': create_ua(),
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',  # Higher quality values
+        'Accept-Encoding': 'gzip, deflate, br, zstd',  # Add zstd
         'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Cache-Control': 'max-age=0',
+        'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"'
     }
